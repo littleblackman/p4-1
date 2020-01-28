0749bd5ed8fc07437970ff45fc81b01fa4e72098
littleBlackMan <littleblackman@users.noreply.github.com>
2020-01-28 10:43:51 +0100

    Update listPostsView.php
    use php syntax alternative

--- view/frontend/listPostsView.php
+++ view/frontend/listPostsView.php
@@ -2,10 +2,7 @@
 <?php ob_start(); ?>
 <?php include 'menu.php'; ?>
 <p class="text-center text-success t4">Dérniers billets du blog </p>
-<?php
-while ($data = $posts->fetch(PDO::FETCH_ASSOC))
-{
-?>
+<?php while ($data = $posts->fetch(PDO::FETCH_ASSOC)) :?>
    <div class='col-md-12 text-justify text-center'>
         <h3 class='t3'>
             <?= htmlspecialchars($data['title']) ?>
@@ -15,10 +12,8 @@ while ($data = $posts->fetch(PDO::FETCH_ASSOC))
            <em><a href="index.php?action=post&amp;id=<?= $data['id'] ?>">Lire l'article</a></em>
         </p>
    </div>
-<?php
-}
-$posts->closeCursor();
-?>
+<?php endwhile;?>
+<?php $posts->closeCursor(); ?>
 <?php include 'footer.php'; ?>
 <?php $content = ob_get_clean(); ?>
 <?php require('template.php'); ?>
