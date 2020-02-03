5f60021909807a74768f479bd56205499f574387
littleBlackMan <littleblackman@users.noreply.github.com>
2020-02-03 13:11:21 +0100

    Update Autoloader.php
    fixe bug on autoload

--- Autoloader.php
+++ Autoloader.php
@@ -2,24 +2,26 @@
 class Autoloader{
 	public static function register()
 	{
-    	//spl_autoload_register(array(__CLASS__, 'autoload'));
-    	spl_autoload_register(function ($class_name) {
-    include $class_name . '.php';
-});
-    }
-	/*public static function autoload($class)
+		
+		// CREER LES CONSTANTES
+		// exemple define('MODEL', 'D:\wamp\www\P4\model\';
+		
+    		spl_autoload_register(array(__CLASS__, 'autoload'));
+	}
+	
+	public static function autoload($class)
 	{
-		if(file_exists(model.$class.'.php'))
+		if(file_exists(MODEL.$class.'.php'))
 		{
-			include_once (model.$class.'.php');
-		}else if(file_exists(index.$class.'.php'))
+			include_once ($class.'.php');
+		}else if(file_exists(CORE.$class.'.php'))
 		{
-			include_once (index.$class.'.php');
-		}else if(file_exists(controller.$class.'.php'))
+			include_once (CORE.$class.'.php');
+		}else if(file_exists(CONTROLLER.$class.'.php'))
 		{
-			include_once (controller.$class.'.php');
+			include_once (CONTROLLER.$class.'.php');
 		}
     	
-    }*/
+    }
  
 }
